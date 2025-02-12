cb0e52e2907422f7c6b466f076259b7071f09649
Ashik9500 <ashikameen790@gmail.com>
2025-02-12 14:49:53 +0530

    second

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Ashik Ameen</h1>
+      <p>Contact: ashik.ameen.s70@kalvium.community</p>
+      <p>Phone: 8970736107</p>
+    </div>
+  );
+}
+
+export default App;
+
